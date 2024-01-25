d7605be6131b268ac3fbfc4a2355d2390fd460e0
katelim215 <katelimosnero215@gmail.com>
2024-01-26 01:46:41 +0800

    task 2 complete

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,5 @@
-let categoriesList = document.getElementById('categories');
-let categories = categoriesList.getElementsByClassName('item');
+// let categoriesList = document.getElementById('categories');
+let categories = document.querySelectorAll('.item');
 console.log('Number of categories:', categories.length);
 
 
@@ -7,7 +7,7 @@ for (let i = 0; i < categories.length; i++) {
   const category = categories[i];
   const header = category.querySelector('h2');
   const elementsList = category.querySelector('ul');
-  const elements = elementsList.getElementsByTagName('li');
+  const elements = elementsList.querySelectorAll('li');
   
   console.log('Category:', header.textContent);
   console.log('Elements:', elements.length);
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.getElementById("ingredients");
+const fragment = document.createDocumentFragment();
+
+
+ingredients.forEach((ingredient) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add("item");
+  fragment.appendChild(listItem);
+});
+
+
+ingredientsList.appendChild(fragment);
+
