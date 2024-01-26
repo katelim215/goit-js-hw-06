5aa7ae4e613fe153e67f94691cdea0ef141b3457
katelim215 <katelimosnero215@gmail.com>
2024-01-26 18:59:36 +0800

    task 3 complete

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,38 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryList = document.querySelector('.gallery');
+
+const markup = images
+  .map((image) => `<li class="gallery-item">
+  <img src="${image.url}" alt="${image.alt}">
+  </li>`)
+  .join("");
+console.log(markup);
+
+galleryList.insertAdjacentHTML("beforeend", markup);
+
+const dynamicStyles = `
+  .gallery {
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  .gallery-item {
+    flex: 1 0 30%;
+    margin: 10px;
+  }
+
+  .gallery-item img {
+    max-width: 100%;
+    height: auto;
+  }
+`;
+
+const styleElement = document.createElement('style');
+styleElement.textContent = dynamicStyles;
+document.head.appendChild(styleElement);
