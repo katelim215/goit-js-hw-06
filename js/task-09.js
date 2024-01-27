2fa4aeb0773f4214e8d0cca5b210cd965ceafa32
katelim215 <katelimosnero215@gmail.com>
2024-01-28 02:06:17 +0800

    task 9 complete

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function changeColor() {
+  const bodyElement = document.body;
+  const colorSpan = document.querySelector('.color');
+  const randomColor = getRandomHexColor();
+
+  bodyElement.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+}
+
+const changeColorBtn = document.querySelector('.change-color');
+changeColorBtn.addEventListener('click', changeColor);
+
