337420c4702cb99651a54a69e9381677df0dd751
katelim215 <katelimosnero215@gmail.com>
2024-01-28 01:48:27 +0800

    task 8 complete

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const emailInput = loginForm.elements.email;
+    const passwordInput = loginForm.elements.password;
+
+    if (emailInput.value === '' || passwordInput.value === '') {
+        alert('All fields must be filled in.');
+    } else {
+        const formData = {
+            email: emailInput.value,
+            password: passwordInput.value,
+        };
+
+        console.log(formData);
+        loginForm.reset();
+    }
+});
