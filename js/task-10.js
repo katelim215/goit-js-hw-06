eb0cf288b5024aeddbc473384d85f384c6097729
katelim215 <katelimosnero215@gmail.com>
2024-01-28 02:34:52 +0800

    task 10 complete

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const input = document.querySelector('input');
+const boxesContainer = document.getElementById('boxes');
+
+function createBoxes(amount) {
+  const baseSize = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    const size = baseSize + i * 10;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+createButton.addEventListener('click', () => {
+  const numberOfBoxes = parseInt(input.value, 10);
+  createBoxes(numberOfBoxes);
+});
+
+destroyButton.addEventListener('click', destroyBoxes);
